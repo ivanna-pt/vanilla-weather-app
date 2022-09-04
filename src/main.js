ae5a1f5c39c23cdfd0ea7741b40cf5256266f624
Ivanna Pokhyl <ivanna.pokhyl@gmail.com>
2022-09-04 21:46:07 +0300

    Feat unit switch main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const fahrenheitBtn = document.querySelector('#fahrenheit-btn');
+const celsiusBtn = document.querySelector('#celsius-btn');
+const unitSwitch = document.querySelector('.weather__unit-switch');
+
+unitSwitch.addEventListener('click', toggleUnits);
+
+function toggleUnits(e){
+    if (e.target.id === 'celsius-btn'){
+        fahrenheitBtn.classList.remove('active');
+        celsiusBtn.classList.add('active');
+    } else if (e.target.id === 'fahrenheit-btn') {
+        celsiusBtn.classList.remove('active');
+        fahrenheitBtn.classList.add('active');
+    }
+}
